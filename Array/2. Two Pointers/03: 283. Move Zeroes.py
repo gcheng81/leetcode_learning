a037be2c00cb2f8819e21208848a4cbd04899093
gcheng81 <90727499+gcheng81@users.noreply.github.com>
2022-07-12 15:11:26 -0400

    Create 03: 283. Move Zeroes.py

---
+++ Array/2. Two Pointers/03: 283. Move Zeroes.py
@@ -0,0 +1,17 @@
+"https://leetcode.com/problems/move-zeroes/"
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # slow: new array index
+        # fast: select new array element
+        slow=0
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[slow]=nums[fast]
+                slow+=1
+    
+        for i in range(slow, len(nums)):
+            nums[i]=0
