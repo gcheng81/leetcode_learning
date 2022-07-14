8e1c2ca7e0255e0a64c20b475a2475d29002978c
gcheng81 <90727499+gcheng81@users.noreply.github.com>
2022-07-14 11:13:24 -0400

    Create 01: 28. Implement strStr().py

---
+++ Array/3. Sliding Window/定长/01: 28. Implement strStr().py
@@ -0,0 +1,13 @@
+"https://leetcode.com/problems/implement-strstr/"
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        start = 0
+        
+        for end in range(len(needle)-1, len(haystack)):
+            if haystack[start:end+1] == needle:
+                return start
+            start += 1
+        
+        return -1
+        
