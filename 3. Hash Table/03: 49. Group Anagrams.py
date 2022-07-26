ba638c817d458f075240a584c57e0a34115abb96
gcheng81 <90727499+gcheng81@users.noreply.github.com>
2022-07-26 10:54:24 -0400

    Update 03: 49. Group Anagrams.py

--- 3. Hash Table/03: 49. Group Anagrams.py
+++ 3. Hash Table/03: 49. Group Anagrams.py
@@ -12,4 +12,19 @@ class Solution:
             
         return ans.values()
 
-# Solution 2: key is Count
+# Solution 2: key is count
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+       
+        for s in strs:
+            
+            count = [0] * 26
+            for char in s:
+                count[ord(char)-ord('a')] += 1
+            
+            key = tuple(count)
+            ans[key].append(s)
+            
+        return ans.values()
+            
