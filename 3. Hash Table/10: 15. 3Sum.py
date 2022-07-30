01ea0ee68bb470425d1d7e7de7b8fd8ead725b35
gcheng81 <90727499+gcheng81@users.noreply.github.com>
2022-07-30 15:25:36 -0400

    Update 10: 15. 3Sum.py

--- 3. Hash Table/10: 15. 3Sum.py
+++ 3. Hash Table/10: 15. 3Sum.py
@@ -2,21 +2,20 @@
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
         n = len(nums)
         if n<3:
             return []
+        nums = sorted(nums)
         ans = []
         
         for i in range(n):
-            left = i+1
-            right = n-1
-            
             if nums[i]>0: # [0, 1, 2, 3, ...]
                 break
             if i>0 and nums[i]==nums[i-1]: #去重i：[有两个完全一样的值] or [0,0,0, ...]
                 continue
             
+            left = i+1
+            right = n-1
             while left<right:
                 total = nums[left]+nums[right]+nums[i]
                 if total==0:
