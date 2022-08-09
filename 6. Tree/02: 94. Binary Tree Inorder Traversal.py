b72bddbed62cf107eb3921f83d4b7b97cdb8fd39
gcheng81 <90727499+gcheng81@users.noreply.github.com>
2022-08-09 15:48:19 -0400

    Update 02: 94. Binary Tree Inorder Traversal.py

--- 6. Tree/02: 94. Binary Tree Inorder Traversal.py
+++ 6. Tree/02: 94. Binary Tree Inorder Traversal.py
@@ -23,3 +23,38 @@ class Solution:
         traversal(root)
         return res
         
+# Solution 2: Iterative Traversal
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # left, mid, right
+        stack = []
+        res = []
+        cur = root
+        
+        while cur or stack:
+            if cur: # 先迭代访问最底层的左子树结点
+                stack.append(cur)
+                cur = cur.left
+            else:  # 到达最左结点后处理栈顶结点
+                cur = stack.pop()
+                res.append(cur.val)
+                cur = cur.right # 取栈顶元素右结点
+        return res
+  
+########## Or:
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # left, mid, right
+        stack = []
+        res = []
+        cur = root
+        
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+        return res    
