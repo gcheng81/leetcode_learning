e1285ed2fdaa16da9a8d200e77d2f4eaa833b576
gcheng81 <90727499+gcheng81@users.noreply.github.com>
2022-08-18 11:16:44 -0400

    Create 18: 222. Count Complete Tree Nodes.py

---
+++ 6. Tree/18: 222. Count Complete Tree Nodes.py
@@ -0,0 +1,31 @@
+"https://leetcode.com/problems/count-complete-tree-nodes/"
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Solution 1: BFS
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        q = collections.deque()
+        #level = []
+        count = 0
+        if root:
+            q.append(root)
+        
+        while q:
+            size = len(q)
+            for _ in range(size):
+                cur = q.popleft()
+                if cur:
+                    #level.append(cur)
+                    count += 1
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+        return count
+        
