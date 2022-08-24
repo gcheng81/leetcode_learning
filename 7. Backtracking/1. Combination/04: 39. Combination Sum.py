e36f9bce12bbc70dea5cccd4223c04292ee88db8
gcheng81 <90727499+gcheng81@users.noreply.github.com>
2022-08-24 16:26:54 -0400

    Create 04: 39. Combination Sum.py

---
+++ 7. Backtracking/1. Combination/04: 39. Combination Sum.py
@@ -0,0 +1,22 @@
+"https://leetcode.com/problems/combination-sum/"
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        
+        def backtracking(path, total, i):
+            if total > target or i>=len(candidates):
+                return
+            
+            if total == target:
+                res.append(path.copy()) # path[:]
+                return
+            
+            path.append(candidates[i])
+            backtracking(path, total+candidates[i], i)
+            path.pop()
+            backtracking(path, total, i+1)
+        
+        if candidates:
+            backtracking([], 0, 0)
+        return res
