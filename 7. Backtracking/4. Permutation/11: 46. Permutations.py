df13db2330b7bff7531f40f58f4c9a4bdb0b2691
gcheng81 <90727499+gcheng81@users.noreply.github.com>
2022-08-29 15:26:44 -0400

    Create 11: 46. Permutations.py

---
+++ 7. Backtracking/4. Permutation/11: 46. Permutations.py
@@ -0,0 +1,27 @@
+"https://leetcode.com/problems/permutations/"
+
+# 排列问题的不同:
+# - 每层都是从0开始搜索而不是startIndex
+# - 需要记录path里已经放了哪些元素
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        def dfs(res, path, nums):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            
+            for i in range(0, len(nums)):
+                if nums[i] in path:
+                    continue
+                path.append(nums[i])
+                dfs(res, path, nums)
+                path.pop()
+            
+        res = []
+        path = []
+        dfs(res, path, nums)
+        return res
+            
+            
