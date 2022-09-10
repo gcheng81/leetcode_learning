56ed613d5675d1a4579fd809d1f6a2dc916582b1
gcheng81 <90727499+gcheng81@users.noreply.github.com>
2022-09-10 16:49:17 -0400

    Update 14: 226. Invert Binary Tree.py

--- 6. Tree/14: 226. Invert Binary Tree.py
+++ 6. Tree/14: 226. Invert Binary Tree.py
@@ -7,7 +7,22 @@
 #         self.left = left
 #         self.right = right
 
-# Solution 1: BFS
+# Solution 1: DFS
+# Recursion
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return
+        # 如果单独抽出一个二叉树节点，它需要做什么事情？Action: switch its left and right
+        # 需要在什么时候（前/中/后序位置）做？前/后
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
+
+    
+# Solution 2: BFS
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         q = collections.deque()
