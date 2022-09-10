24369f07b6d2fecbd73fad9533635e42f5efd752
gcheng81 <90727499+gcheng81@users.noreply.github.com>
2022-09-10 17:33:21 -0400

    Update 10: 116. Populating Next Right Pointers in Each Node.py

--- 6. Tree/10: 116. Populating Next Right Pointers in Each Node.py
+++ 6. Tree/10: 116. Populating Next Right Pointers in Each Node.py
@@ -1,1 +1,29 @@
 "https://leetcode.com/problems/populating-next-right-pointers-in-each-node/"
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+# Solution1: DFS: Recursion
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        def connect_two_nodes(node1, node2):
+            if node1 is None or node2 is None:
+                return
+            node1.next = node2
+            # same node
+            connect_two_nodes(node1.left, node1.right)
+            connect_two_nodes(node2.left, node2.right)
+            # different node
+            connect_two_nodes(node1.right, node2.left)
+        
+        if root is None:
+            return
+        connect_two_nodes(root.left, root.right)
+        return root
+        
