37a44011876280f612dcce3de5d0c4fb5f0b2f6a
gcheng81 <90727499+gcheng81@users.noreply.github.com>
2022-09-12 10:57:33 -0400

    Update 04: 102. Binary Tree Level Order Traversal.py

--- 6. Tree/2. BFS/04: 102. Binary Tree Level Order Traversal.py
+++ 6. Tree/2. BFS/04: 102. Binary Tree Level Order Traversal.py
@@ -8,18 +8,20 @@
 #         self.left = left
 #         self.right = right
 
-# Solution 1: Iteration
+# Solution 1: BFS: Iteration
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
         res = []
         q = collections.deque()
-        if root:
-            q.append(root)
+        q.append(root)
         
         while q:
-            size = len(q) 
             level = []
-            for _ in range(size): # len(q) will change, so we need size
+            size = len(q)
+            for _ in range(size):# len(q) will change, so we need size
                 cur = q.popleft()
                 if cur:
                     level.append(cur.val)
@@ -27,10 +29,9 @@ class Solution:
                     q.append(cur.left)
                 if cur.right:
                     q.append(cur.right)
-        
             if level:
                 res.append(level)
-        
         return res
+               
       
  # Solution 2: Recursion
