c77390acc48bed9ddc004e9f4d7971842e6e1724
gcheng81 <90727499+gcheng81@users.noreply.github.com>
2022-09-13 15:45:33 -0400

    Update 05: 40. Combination Sum II.py

--- 7. Backtracking/1. Combination/05: 40. Combination Sum II.py
+++ 7. Backtracking/1. Combination/05: 40. Combination Sum II.py
@@ -1,29 +1,30 @@
 "https://leetcode.com/problems/combination-sum-ii/"
 
-
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         res = []
-        
-        def backtracking(start_index, target, path):
-            if target < 0:
-                return
-            
-            if target == 0:
-                res.append(path.copy())
-                return
-            
-            prev = -1    
-            for i in range(start_index, len(candidates)):
-                if candidates[i] == prev:
-                    continue
-                
-                path.append(candidates[i])
-                backtracking(i+1, target-candidates[i], path)
-                path.pop()
-                prev = candidates[i]
-        
-        if candidates:
-            backtracking(0, target, [])
+        path = []
+        start_index = 0
+        total = 0
+        self.backtracking(res, path, start_index, candidates, target, total)
         return res
+        
+    def backtracking(self, res, path, start_index, nums, target, total):
+        if total > target:
+            return
+        
+        elif total == target:
+            res.append(path[:])
+            return
+        
+        else:
+            for i in range(start_index, len(nums)):
+                if i>start_index and nums[i-1] == nums[i]:
+                    continue
+            
+                path.append(nums[i])
+                total += nums[i]
+                self.backtracking(res, path, i+1, nums, target, total)
+                path.pop()
+                total -= nums[i]
